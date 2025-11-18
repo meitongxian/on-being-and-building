e2143c5e3c5a6291e460f4ba7e0b6b0e9c69e8a8
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2025-11-17 22:38:23 -0500

    squares

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const grid = document.querySelectorAll(".grid > div");
 grid.forEach(sky => {
     sky.addEventListener("mouseover", function() {
         console.log("hi");
-        var offsetX = Math.floor(Math.random() * (20 + 20 + 1)) -20;
-        var offsetY = Math.floor(Math.random() * (20 + 20 + 1)) -20;
+        var offsetX = Math.floor(Math.random() * (50 + 50 + 1)) -50;
+        var offsetY = Math.floor(Math.random() * (50 + 50 + 1)) -50;
         console.log(offsetX, offsetY);
-        this.style.transform = "translateX(" + offsetX + "px)";
-        this.style.transform = "translateY(" + offsetY + "px)";
+        this.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
     });
 });
 
@@ -16,10 +15,9 @@ const title = document.querySelectorAll(".title > h1 > span");
 title.forEach(word => {
     word.addEventListener("mouseover", function() {
         console.log("hi");
-        var offsetX = Math.floor(Math.random() * (30 + 30 + 1)) -30;
-        var offsetY = Math.floor(Math.random() * (30 + 30 + 1)) -30;
+        var offsetX = Math.floor(Math.random() * (50 + 50 + 1)) -50;
+        var offsetY = Math.floor(Math.random() * (50 + 50 + 1)) -50;
         console.log(offsetX, offsetY);
-        this.style.transform = "translateX(" + offsetX + "px)";
-        this.style.transform = "translateY(" + offsetY + "px)";
+        this.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
     });
 });
