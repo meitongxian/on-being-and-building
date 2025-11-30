daca58e215a5b2227eb07710d984449272b93163
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2025-11-30 16:59:06 -0500

    scrollbar test

--- script.js
+++ script.js
@@ -60,9 +60,49 @@ mobileResize();
 
 function mobileResize() {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     mobileResize();
+    customScroll();
 });
+
+
+//scrollbar 
+customScroll();
+
+function customScroll() {
+    const scrollBar = document.querySelector(".scrollbar");
+    scrollBar.style.setProperty("width", "calc(" + window.innerWidth + "px - 4rem)"); // set to 100%
+    let totalBarWidth = scrollBar.offsetWidth; // get 100% width
+
+    const mainContent = document.querySelector("main");
+    const totalWidth = mainContent.offsetWidth - window.innerWidth; // get total width of entire page
+
+    let offsetX = window.pageXOffset;
+    let fraction = offsetX / totalWidth; // get fraction of page scrolled
+
+    scrollBarWidth = fraction * totalBarWidth;
+    scrollBar.style.setProperty("width", scrollBarWidth + "px"); // set to new width
+
+    const timestamp = document.querySelector("#timestamp");
+    let minutes = Math.floor((fraction * 2065) / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    let seconds = Math.ceil((fraction * 2065) % 60);
+    if (seconds == 60) {
+        seconds = "00";
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    timestamp.innerHTML = minutes + ":" + seconds;
+}
+
+document.addEventListener('scroll', () => {
+    customScroll();
+});
+
+
